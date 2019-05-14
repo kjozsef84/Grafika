5404bf4f712282c8e3914225f3999a35a3726cf6
Luke <lukacslev95@gmail.com>
2019-05-14 15:07:35 +0300

    Added missing dll

--- QtFramework/Core/bicubicbsplinepatch.h
+++ QtFramework/Core/bicubicbsplinepatch.h
@@ -2,22 +2,22 @@
 
 namespace cagd {
 
-class BicubicBSplinePatch : public TensorProductSurface3 {
+class BicubicBSplinePatch : public TensorProductSurface3
+{
 
 public:
+  BicubicBSplinePatch();
+  GLboolean UBlendingFunctionValues(GLdouble u_knot,
+                                    RowMatrix<GLdouble>& blending_values) const;
 
-    BicubicBSplinePatch();
-    GLboolean UBlendingFunctionValues(
-            GLdouble u_knot, RowMatrix<GLdouble>& blending_values) const;
+  GLboolean VBlendingFunctionValues(GLdouble v_knot,
+                                    RowMatrix<GLdouble>& blending_values) const;
 
-    GLboolean VBlendingFunctionValues(
-            GLdouble v_knot, RowMatrix<GLdouble>& blending_values) const;
-
-   GLboolean CalculatePartialDerivatives(
-            GLuint maximum_order_of_partial_derivatives,
-            GLdouble u, GLdouble v, PartialDerivatives& pd) const;
-
-
+  GLboolean CalculatePartialDerivatives(
+    GLuint maximum_order_of_partial_derivatives,
+    GLdouble u,
+    GLdouble v,
+    PartialDerivatives& pd) const;
 };
 
 }
